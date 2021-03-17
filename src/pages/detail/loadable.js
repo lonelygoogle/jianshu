837846d7d491197dac7b2829e69549c431e6ea86
huangsiqin <450555286@qq.com>
2021-03-17 11:20:34 +0800

    dockerfile

--- src/pages/detail/loadable.js
+++ src/pages/detail/loadable.js
@@ -1,5 +1,5 @@
 import Loadable from 'react-loadable';
-import React, { Component } from 'react'
+import React from 'react'
 // import Loading from './my-loading-component';
 
 const LoadableComponent = Loadable({
